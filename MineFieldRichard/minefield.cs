8f158926c2cdd28f86555c9bbdf612055f943249
zazaman <a28283878@gmail.com>
2016-07-11 23:08:13 +0800

    Titansoft TDD practice first commit

---
+++ MineFieldRichard/minefield.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineFieldRichard
+{
+    public class minefield
+    {
+        public char[,] makeMap(int row, int col)
+        {
+            char[,] map = new char[row, col];
+            for(int i = 0; i < row; i++)
+            {
+                for(int k = 0; k < col; k++)
+                {
+                    map[i,k] = '*';
+                }
+            }
+
+            return map;
+        }
+
+        public char[,] mineHint(char[,] mineMap)
+        {
+            int row = mineMap.GetLength(0), col = mineMap.GetLength(1);
+            int[,] mineHintMap = new int[row, col];
+            char[,] output = new char[row, col];
+            for (int i = 0; i < row; i++)
+            {
+                for (int k = 0; k < col; k++)
+                {
+                    mineHintMap[i, k] = 0;
+                }
+            }
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int k = 0; k < col; k++)
+                {
+                    if (mineMap[i, k] != '*')
+                    {
+                        mineHintMap[i, k] = numOfMine(mineMap, i, k, mineHintMap[i, k]);
+                    }
+                    else
+                    {
+                        mineHintMap[i, k] = -1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int k = 0; k < col; k++)
+                {
+                    if (mineHintMap[i, k] == -1)
+                    {
+                        output[i, k] = '*';
+                    }
+                    else output[i, k] = (char)(mineHintMap[i, k] + 48);
+                }
+            }
+
+            return output;
+        }
+        public int numOfMine(char[,] mineMap, int row, int col, int sum)
+        {
+            int sumOfMine = sum;//九宮格內的地雷數量
+            int I, K;
+            if (row - 1 < 0) I = 0;
+            else I = row - 1;
+
+            if (col - 1 < 0) K = 0;
+            else K = col - 1;
+            for (int i = I; i < mineMap.GetLength(0) && i < row + 2; i++)
+            {
+                for (int k = K; k < mineMap.GetLength(1) && k < col + 2; k++)
+                {
+                    if (i == row && k == col) continue;
+                    if (mineMap[i, k] == '*') sumOfMine++;
+                }
+            }
+
+            return sumOfMine;
+        }
+    }
+}
---
+++ MineFieldTest/minefieldtest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using MineFieldRichard;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MineFieldTest
+{
+    [TestClass]
+    public class minefieldtest
+    {
+        int row, col;
+        minefield mf = new minefield();
+        [TestMethod]
+        public void makeMap()
+        {
+            row = 3;
+            col = 3;
+            //act
+            char[,]  expect = new char[3, 3] { { '*', '*', '*' }, { '*', '*', '*' }, { '*', '*', '*' } };
+            
+            //do
+            char[,] actual = mf.makeMap(row,col);
+
+            //Assert
+            CollectionAssert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void MapInput()
+        {
+            row = 3;
+            col = 4;
+            //act
+            char[,] mineMap = new char[3, 4] { { '*', '.', '.', '.' }, { '.', '.', '*', '.' }, { '.', '.', '.', '.' } };
+            char[,] expect = new char[3, 4] { { '*', '2', '1', '1' }, { '1', '2', '*', '1' }, { '0', '1', '1', '1' } };
+            //do
+            char[,] actual = mf.mineHint(mineMap);
+
+            //Assert
+            CollectionAssert.AreEqual(expect, actual);
+        }
+    }
+}
